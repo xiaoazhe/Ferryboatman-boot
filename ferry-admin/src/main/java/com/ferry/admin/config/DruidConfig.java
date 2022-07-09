ce8bd60769c936fdebdfa1e0fa545075a455397d
摆渡人 <1540035958@qq.com>
2022-07-09 16:33:32 +0800

    delete

--- ferry-admin/src/main/java/com/ferry/admin/config/DruidConfig.java
+++ ferry-admin/src/main/java/com/ferry/admin/config/DruidConfig.java
@@ -67,16 +67,11 @@ public class DruidConfig {
     @ConditionalOnMissingBean
     public ServletRegistrationBean <Servlet> druidServlet() {
         ServletRegistrationBean <Servlet> servletRegistrationBean = new ServletRegistrationBean <Servlet>(new StatViewServlet(), "/druid/*");
-
-        //白名单：
-//        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
-        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
-        //servletRegistrationBean.addInitParameter("deny","192.168.1.119");
         //登录查看信息的账号密码, 用于登录Druid监控后台
         servletRegistrationBean.addInitParameter("loginUsername", "admin");
         servletRegistrationBean.addInitParameter("loginPassword", "admin");
         //是否能够重置数据.
-        servletRegistrationBean.addInitParameter("resetEnable", "true");
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
         return servletRegistrationBean;
 
     }
