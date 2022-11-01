089f784186509c9af022228c6f3500f8ec60bf28
摆渡人 <1540035958@qq.com>
2022-11-01 22:40:29 +0800

    修改web blog顺序

--- ferry-web/src/main/java/com/ferry/web/service/impl/BlogServiceImpl.java
+++ ferry-web/src/main/java/com/ferry/web/service/impl/BlogServiceImpl.java
@@ -75,7 +75,7 @@ public class BlogServiceImpl extends ServiceImpl <BlBlogMapper, BlBlog> implemen
             queryWrapper.eq(BlBlog.COL_TYPE_ID, pageRequest.getEnabled());
         }
         queryWrapper.eq(BlBlog.COL_IS_PUBLISH, "1");
-        queryWrapper.orderByDesc(BlBlog.COL_CREATE_TIME);
+        queryWrapper.orderByDesc(BlBlog.COL_SORT);
         Page<BlBlog> typePage = blogMapper.selectPage(page, queryWrapper);
         PageResult pageResult = new PageResult(typePage);
         return pageResult;
