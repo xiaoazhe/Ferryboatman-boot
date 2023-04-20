7cffeeaf531f5a2830f3daaed31a6661dacdb13f
摆渡人 <1540035958@qq.com>
2023-04-20 21:35:45 +0800

    update

--- ferry-web/src/main/java/com/ferry/web/WebApplication.java
+++ ferry-web/src/main/java/com/ferry/web/WebApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -37,8 +38,8 @@ public class WebApplication {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-//    @Bean
-//    public BCryptPasswordEncoder encoder() {
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
